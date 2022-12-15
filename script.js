b102da9018f19f2463714f4a2e83ce0e42e6fb3e
Youssefidry <89986853+Youssefidry@users.noreply.github.com>
2022-12-15 14:07:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+
+
+
+
+
